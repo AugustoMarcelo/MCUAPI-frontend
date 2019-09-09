b852a238c31e5709bc950d6c63dd562fed0e655c
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-09 17:19:01 -0300

    Disable props rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
+    'react/jsx-props-no-spreading': 'off',
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: ['tron'] }]
   },

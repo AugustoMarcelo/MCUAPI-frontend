d41e5e1bf3897d3543ebf9a38872868e32cdd9ed
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-10 19:18:54 -0300

    Create validation for form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Type a valid e-mail')
+    .required('This field is required'),
+  password: Yup.string().required('This field is required'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MCUAPI Logo" width={100} height={100} />
-      <form>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
 
         <button type="submit">Enter</button>
-      </form>
+      </Form>
     </>
   );
 }

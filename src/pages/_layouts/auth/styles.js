675789e29b965cbe04a741b02675c497aed4c2d1
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-10 16:18:09 -0300

    Created style for auth layouts

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,54 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(180deg, #0984e3, #74b9ff);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.2);
+      border: 0;
+      height: 44px;
+      padding: 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+
+      &:focus {
+        background: rgba(0, 0, 0, 0.3);
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #e17055;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.08, '#e17055')};
+      }
+    }
+  }
 `;

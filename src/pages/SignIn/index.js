a4d5384cfcd9474c2dd1733efd9f4d4daa04245e
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-10 16:15:25 -0300

    Create login form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import logo from '~/assets/logo.svg';
+
 export default function SignIn() {
-  return <h1>Sign In</h1>
+  return (
+    <>
+      <img src={logo} alt="MCUAPI Logo" width={100} height={100} />
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+
+        <button type="submit">Enter</button>
+      </form>
+    </>
+  );
 }

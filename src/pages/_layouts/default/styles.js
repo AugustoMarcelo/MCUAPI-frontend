451088a85f7aa435cb29cf37bedf6bf9a39c437e
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-12 16:35:01 -0300

    Changing margin and height

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  min-height: 100%;
   background: #f2f2f2;
   display: flex;
 `;
 
 export const Content = styled.div`
+  width: 100%;
+  margin-left: 130px;
   padding: 10px;
 `;

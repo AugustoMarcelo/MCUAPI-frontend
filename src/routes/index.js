f549ed6daff6f494bebe695cffdfdec6b4debf05
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-14 16:27:24 -0300

    Added tvshows route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Route from './Route';
 import SignIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
 import Movies from '~/pages/Movies';
+import TVShows from '~/pages/TVShows';
 
 export default function Routes() {
   return (
@@ -13,6 +14,7 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/movies" component={Movies} isPrivate />
+      <Route path="/tvshows" component={TVShows} isPrivate />
     </Switch>
   );
 }

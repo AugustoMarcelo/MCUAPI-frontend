9c0d60b0398920d771a6f930a0b2b0a1ae4057dd
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-17 21:48:20 -0300

    Using Context API to check provider info about RightSidebar status

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import { Wrapper, Content } from './styles';
 import LeftSidebar from '~/components/LeftSidebar';
+import { RightSidebarContext } from '~/services/RightSidebarContext';
+import RightSidebar from '~/components/RightSidebar';
 
 export default function DefaultLayout({ children }) {
+  const [rightSidebarStatus, setRightSidebarStatus] = useState({
+    open: false,
+    elementId: null,
+  });
+
+  const providerRightSidebarStatus = useMemo(
+    () => ({ rightSidebarStatus, setRightSidebarStatus }),
+    [rightSidebarStatus, setRightSidebarStatus]
+  );
+
   return (
     <Wrapper>
-      <LeftSidebar />
-      <Content>{children}</Content>
+      <RightSidebarContext.Provider value={providerRightSidebarStatus}>
+        <LeftSidebar />
+        <Content open={rightSidebarStatus.open}>{children}</Content>
+        <RightSidebar />
+      </RightSidebarContext.Provider>
     </Wrapper>
   );
 }
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
   min-height: 100%;
@@ -10,4 +10,11 @@ export const Content = styled.div`
   width: 100%;
   margin-left: 130px;
   padding: 10px;
+  transition: 0.2s ease-in;
+
+  ${props =>
+    props.open &&
+    css`
+      margin-right: 300px;
+    `}
 `;

4184a738b968b4ddb0bf791163ad3a84b5d7e37a
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-23 22:25:33 -0300

    Changing close sidebar icon

--- src/components/RightSidebar/index.js
+++ src/components/RightSidebar/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
 
 import { Container, CloseButton } from './styles';
 import { RightSidebarContext } from '~/services/RightSidebarContext';
@@ -17,7 +17,7 @@ export default function RightSidebar() {
   return (
     <Container open={open}>
       <CloseButton onClick={closeSidebar}>
-        <FaTimes size={24} />
+        <FaArrowRight size={24} />
       </CloseButton>
     </Container>
   );

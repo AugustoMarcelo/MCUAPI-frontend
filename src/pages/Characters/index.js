9abe923c635022003179cd30fdc9716c479c9915
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-23 22:26:01 -0300

    Creating Characters page (in progress)

---
+++ src/pages/Characters/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { FaPencilAlt, FaRegTrashAlt } from 'react-icons/fa';
+
+import api from '~/services/api';
+
+import FAB from '~/components/FAB';
+import { Grid, Card, HeroName, Name, Actions } from './styles';
+
+export default function Characters() {
+  const [characters, setCharacters] = useState({
+    count: 0,
+    data: [],
+  });
+
+  useEffect(() => {
+    async function loadCharacters() {
+      const response = await api.get('characters');
+
+      setCharacters({ ...response.data });
+    }
+
+    loadCharacters();
+  }, []);
+
+  function addCharacter() {
+    console.tron.log('Add Character');
+  }
+
+  return (
+    <>
+      <Grid>
+        {characters.data.map(character => (
+          <Card key={String(character.id)}>
+            <img src={character.photo_url} alt={`${character.name}`} />
+            <HeroName>{character.name.split('/', 1)}</HeroName>
+            <Name>{character.name.split('/')[1]}</Name>
+            <Actions>
+              <button type="button">
+                <FaPencilAlt size={18} />
+              </button>
+              <button type="button">
+                <FaRegTrashAlt size={18} color="#f0131e" />
+              </button>
+            </Actions>
+          </Card>
+        ))}
+      </Grid>
+      <FAB onClick={addCharacter} />
+    </>
+  );
+}

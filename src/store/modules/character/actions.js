4730a692b734daf98e991a204247f972541c0b63
AugustoMarcelo <mrclgst10@gmail.com>
2019-09-27 13:43:34 -0300

    Added a action to update a character

--- src/store/modules/character/actions.js
+++ src/store/modules/character/actions.js
@@ -4,3 +4,10 @@ export function addRequest(data) {
     payload: { data },
   };
 }
+
+export function updateRequest(data, id) {
+  return {
+    type: '@character/UPDATE_REQUEST',
+    payload: { data, id },
+  };
+}

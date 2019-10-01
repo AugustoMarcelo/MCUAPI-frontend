50ba2d4a7889b76cf67738f665ac996fc1628e0a
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-01 20:54:38 -0300

    Adding movie sagas

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -3,7 +3,8 @@ import { all } from 'redux-saga/effects';
 import auth from './auth/sagas';
 import user from './user/sagas';
 import character from './character/sagas';
+import movie from './movie/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, character]);
+  return yield all([auth, user, character, movie]);
 }

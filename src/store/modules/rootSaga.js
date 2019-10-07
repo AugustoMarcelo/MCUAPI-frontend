3891028d5f01270aba3a818a84f34fc00853090e
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-07 17:16:49 -0300

    Adding tvshow sagas

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -4,7 +4,8 @@ import auth from './auth/sagas';
 import user from './user/sagas';
 import character from './character/sagas';
 import movie from './movie/sagas';
+import tvshow from './tvshow/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, character, movie]);
+  return yield all([auth, user, character, movie, tvshow]);
 }

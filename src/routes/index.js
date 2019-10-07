ebd91410116bd99224bf3d45a604f29cce00e1f4
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-07 17:15:56 -0300

    Created routes to add and edit tvshows

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,7 @@ import TVShows from '~/pages/TVShows';
 import Characters from '~/pages/Characters';
 import ManageCharacter from '~/pages/ManageCharacter';
 import ManageMovie from '~/pages/ManageMovie';
+import ManageTvshow from '~/pages/ManageTvshow';
 
 export default function Routes() {
   return (
@@ -19,6 +20,13 @@ export default function Routes() {
       <Route path="/movies/create" exact component={ManageMovie} isPrivate />
       <Route path="/movies/edit/:id" exact component={ManageMovie} isPrivate />
       <Route path="/movies" component={Movies} isPrivate />
+      <Route path="/tvshows/create" exact component={ManageTvshow} isPrivate />
+      <Route
+        path="/tvshows/edit/:id"
+        exact
+        component={ManageTvshow}
+        isPrivate
+      />
       <Route path="/tvshows" component={TVShows} isPrivate />
       <Route
         path="/characters/create"
